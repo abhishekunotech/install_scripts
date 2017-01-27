38bcd9821590e4c414007bd767d1604d8ac12fb6
ak29 <ak229@buffalo.edu>
2017-01-27 11:07:34 +0530

    Moved the Environment variables of Hadoop to a separate shell script, will call it as a function by sourcing it in the original script

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -17,22 +17,8 @@ wget http://redrockdigimark.com/apachemirror/hadoop/common/hadoop-2.7.3/hadoop-2
 
 tar -zxvf hadoop-2.7.3.tar.gz
 
-cat <<EOF >>~/.bashrc
-#Hadoop Enviroment Variables Starts
-export HADOOP_INSTALL=/opt/hadoop/hadoop-2.7.3
-export HADOOP_PREFIX=/opt/hadoop/hadoop-2.7.3
-export HADOOP_HOME=/opt/hadoop/hadoop-2.7.3
-export HADOOP_MAPRED_HOME=$HADOOP_HOME
-export HADOOP_COMMON_HOME=$HADOOP_HOME
-export HADOOP_HDFS_HOME=$HADOOP_HOME
-export YARN_HOME=$HADOOP_HOME
-export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-export PATH=$HADOOP_INSTALL/bin:$PATH
-export PATH=$HADOOP_INSTALL/sbin:$PATH
-#Hadoop Enviroment Variables Ends
-EOF
-
-source ~/.bashrc
+curl https://raw.githubusercontent.com/abhishekunotech/install_scripts/master/setJavaEnv.sh > setJavaEnv.sh
+source setHadoopEnv.sh
 
 hadoop version
 
---
+++ setHadoopEnv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Now we set the Environment Variables for Hadoop 2.7.3"
+
+#Hadoop Enviroment Variables Starts
+export HADOOP_INSTALL=/opt/hadoop/hadoop-2.7.3
+export HADOOP_PREFIX=/opt/hadoop/hadoop-2.7.3
+export HADOOP_HOME=/opt/hadoop/hadoop-2.7.3
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export PATH=$HADOOP_INSTALL/bin:$PATH
+export PATH=$HADOOP_INSTALL/sbin:$PATH
+#Hadoop Enviroment Variables Ends
