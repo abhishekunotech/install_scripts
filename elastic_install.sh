7cada68cff5c6be07944e5c6a93f3982efab850d
abhishekunotech <abhishek.kulkarni@unotechsoft.com>
2017-02-16 00:01:40 +0530

    Edited a tab issue

--- elastic_install.sh
+++ elastic_install.sh
@@ -8,11 +8,11 @@ rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
 
 cat <<EOF >>/etc/yum.repos.d/elasticsearch.repo
 [elasticsearch-2.x]
-	name=Elasticsearch repository for 2.x packages
-	baseurl=http://packages.elastic.co/elasticsearch/2.x/centos
-	gpgcheck=1
-	gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
-	enabled=1	
+name=Elasticsearch repository for 2.x packages
+baseurl=http://packages.elastic.co/elasticsearch/2.x/centos
+gpgcheck=1
+gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+enabled=1	
 EOF
 
 yum install -y elasticsearch
