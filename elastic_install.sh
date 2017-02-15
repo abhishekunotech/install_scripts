337229be2f48371e7410d15a4b1384035ea91a43
abhishekunotech <abhishek.kulkarni@unotechsoft.com>
2017-02-15 23:52:56 +0530

    Elastic install script

---
+++ elastic_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+yum install java-1.7.0-openjdk.x86_64
+yum install -y wget vim
+
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+
+
+cat <<EOF >>/etc/yum.repos.d/elasticsearch.repo
+[elasticsearch-2.x]
+	name=Elasticsearch repository for 2.x packages
+	baseurl=http://packages.elastic.co/elasticsearch/2.x/centos
+	gpgcheck=1
+	gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+	enabled=1	
+EOF
+
+yum install -y elasticsearch
+
+cd /etc/elasticsearch/
+
+cat <<EOF >> elasticsearch.yaml
+network.host: 0.0.0.0
+EOF
+
+curl -XGET 'localhost:9200'
